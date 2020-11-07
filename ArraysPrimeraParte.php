5c3d88995c3f4e24723a49a6712fc1bb486de6eb
Adriana Rodriguez <adri_rgz_87@hotmail.com>
2020-11-07 16:59:01 +0000

    actualizacion de Arrays

---
+++ ArraysPrimeraParte.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Arrays Primera Parte</title>
+  </head>
+  <body>
+    <?php
+    //Creamos el array de la Liga Endesa de Basket temporada 2016-2017
+     $EndesaBasket=[
+       "Baloncesto Sevilla",
+       "CAI Zaragoza",
+       "Divina Seguros Joventut",
+       "Dominion Bilbao Basket",
+       "FC Barcelona Lassa",
+       "Herbalife Gran Canaria",
+       "Iberostar Tenerife",
+       "ICL Manresa",
+       "Laboral KutxaBaskonia",
+       "Montakit Fuenlabrada",
+       "MoraBanc Andorra",
+       "Movistar Estudiantes",
+       "Real Madrid",
+       "Rio Natura MonbusObradoiro",
+       "Unicaja",
+       "UCAM Murcia CB",
+       "Valencia Basket Club",
+     ];
+     //Imorimimos el Array usando print_r()
+     print_r ($EndesaBasket);
+     ?>
+  </body>
+</html>
